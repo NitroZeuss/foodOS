34017359f242fd88a1dc93e461173e7ae9da6c60
NitroZeuss <142437589+NitroZeuss@users.noreply.github.com>
2025-06-28 18:02:03 +0530

    added routing

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import IngredientViewSet, RecipeViewSet, RecipeCategoryViewSet
+from .views import IngredientViewSet, RecipeViewSet, RecipeCategoryViewSet, DietTypeViewSet, RecipeRatingViewSet, RecipeBookmarkViewSet, RecipeCommentViewSet
 
 router = DefaultRouter()
 router.register(r'ingredients', IngredientViewSet)
 router.register(r'recipes', RecipeViewSet)
 router.register(r'recipe-categories', RecipeCategoryViewSet)
+router.register(r'diet-types', DietTypeViewSet)
+router.register(r'recipe-ratings', RecipeRatingViewSet)
+router.register(r'recipe-bookmarks', RecipeBookmarkViewSet)
+router.register(r'recipe-comments', RecipeCommentViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
